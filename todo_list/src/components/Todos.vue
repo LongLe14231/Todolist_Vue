0fe81330a799c593902f635b1ff4ef61ab18c4f0
Le Dinh Minh Long <jason@les-macbook-air.home>
2019-11-28 13:27:52 +1030

    adding source

---
+++ todo_list/src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div v-bind:key="x.id" v-for="x in todos">
+            
+            <TodosItem v-bind:todo="x" v-on:del-todo="$emit('del-todo',x.id)"/>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import TodosItem from './TodosItem.vue';
+    export default {
+        name: "Todos",
+        components:{
+            TodosItem
+        },
+        props: ["todos"]
+    }
+</script>
+
+<style scoped>
+    
+</style>
